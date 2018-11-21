217e01196898cd1e7dfae8f8ce726f4cf6ac6149
Gustavo Restrepo <gustavo.restrepo@utp.edu.co>
2018-11-20 21:12:53 -0500

    Avanzando

---
+++ js/bucles.js
@@ -0,0 +1,16 @@
+'use strict'
+var numero = 100;
+
+//Bucle for
+/*
+for(var i=0; i<numero; i++){
+  console.log(i);
+}
+
+
+//Bucle while
+while (numero < 109) {
+  console.log("Hola pirobito");
+  numero +=1;
+}
+*/
---
+++ js/ventanas_alertas.js
@@ -0,0 +1,14 @@
+'use strict'
+/*
+//Alerta
+alert("Esta es mi alerta");
+alert("JAJAJAJAJ");
+
+//Alerta de confirmacion
+var c = confirm("¿esta sefuro que desea continuar?");
+console.log(c);
+
+//Ingreso de datos
+var edad = prompt("¿Qué edad tienes?", 15);
+console.log(edad);
+*/
